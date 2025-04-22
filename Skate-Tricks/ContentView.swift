1ef78060bb97d757aff53423107de873ac33a5e0
Brandon Whisnant <bc.whisnant@gmail.com>
2025-04-21 21:49:05 -0400

    first commit

---
+++ Skate-Tricks/ContentView.swift
@@ -0,0 +1,116 @@
+//
+//  ContentView.swift
+//  Skate-Tricks
+//
+//  Created by bumblebee on 3/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var flipTricks = [
+        "pop shove it",
+        "180",
+        "frontside 180",
+        "backside 180",
+        "kickflip",
+        "heelflip",
+        "varial flip",
+        "varial heel",
+        "360 flip",
+        "backside bigspin",
+        "frontside bigspin",
+        "frontside flip",
+        "backside flip",
+        "frontside heelflip",
+        "backside heelflip",
+        "laser flip",
+        "frontside 360",
+        "backside 360",
+        "backside 360 kickflip",
+        "frontside 360 kickflip",
+        "backside 360 heelflip",
+        "frontside 360 heelflip",
+        "hardflip"
+    ]
+    
+    @State var stances = [
+        "",
+        "switch",
+        "fakie",
+        "nollie"
+    ]
+
+    @State var grinds = [
+        "boardslide",
+        "50-50",
+        "5-0",
+        "nosegrind",
+        "crook",
+        "smith",
+        "feeble",
+        "overcrook",
+        "suski",
+        "salad",
+        "blunt",
+        "noseblunt",
+        "noseslide",
+        "tailslide",
+        "lipslide",
+    ]
+    
+    @State var stance = ""
+    @State var trick = ""
+    @State var chosenTrick = ""
+    @State var chosenGrind = ""
+    
+    var body: some View {
+        TabView {
+            VStack {
+                
+                Text(chosenTrick != "" ? "\(chosenTrick.uppercased())" : "Pick a Random Trick").font(.title3).padding(.bottom)
+                Button(action: generateRandomTrick) {
+                    Text("Generate Random Trick").font(.title2)
+                }.buttonStyle(.bordered)
+            }
+            .padding()
+            .tabItem {
+                Image(systemName: "1.circle")
+                Text("Flip Tricks")
+            }
+            .tag(1)
+            VStack {
+                Text(chosenGrind != "" ? "\(chosenGrind.uppercased())" : "Pick a Random Grind").font(.title3).padding(.bottom)
+                Button(action: generateRandomGrind) {
+                    Text("Generate Random Grind").font(.title2)
+                }.buttonStyle(.bordered)
+            }
+            .padding()
+            .tabItem {
+                Image(systemName: "2.circle")
+                Text("Grinds")
+            }
+            .tag(2)
+        }
+        
+        
+        
+    }
+    
+    func generateRandomTrick() {
+        stance = stances.randomElement()!
+        trick = flipTricks.randomElement()!
+        chosenTrick = stance + " " + trick
+    }
+    
+    func generateRandomGrind() {
+        stance = stances.randomElement()!
+        trick = grinds.randomElement()!
+        chosenGrind = stance + " " + trick
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skate-Tricks/Skate_TricksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skate_TricksApp.swift
+//  Skate-Tricks
+//
+//  Created by bumblebee on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct Skate_TricksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
